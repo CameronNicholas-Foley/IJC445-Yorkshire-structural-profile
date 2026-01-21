7842ac0ce32346083c3380015f72ef8c02033afb
Cameron Nicholas-Foley <cnicholas-foley1@sheffield.ac.uk>
2026-01-21 11:03:48 +0000

    Add files via upload

---
+++ scripts/1_libraries_IJC445.R
@@ -0,0 +1,17 @@
+################################################################################
+#Libraries required (load all before continuing)
+################################################################################
+
+
+#Data processing and ggplot
+library(tidyverse)
+library(readxl)
+library(janitor)
+library(ggrepel)
+
+#Visualisation themes
+library(ggthemes)
+
+#For Figure 4 Scaling and PCA biplot
+library(scales)
+library(factoextra)
